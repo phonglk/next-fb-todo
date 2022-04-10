b3071e02cfbd0f0e02e97e986d9ff261f5060f8e
Phong Le <phong.lekhoi@gmail.com>
2022-04-10 23:31:53 +0800

    Add Spinner for Add action

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import _ from 'lodash';
 import React, {
   ChangeEventHandler,
@@ -6,6 +7,7 @@ import React, {
   useState,
 } from 'react';
 import AddBtn from './AddBtn';
+import Spinner from './Spinner';
 import { TodoItemObject } from './TodoItem';
 
 const genId = () => `${new Date().getTime()}_${_.uniqueId()}`;
@@ -13,11 +15,15 @@ const genId = () => `${new Date().getTime()}_${_.uniqueId()}`;
 export default function Input(props: {
   addTodo: (todo: TodoItemObject) => Promise<void>;
 }) {
-  const [inputValue, setInputValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (isLoading) return;
     setInputValue(event.target.value);
+    setError('');
   };
 
   const onInputKeydown: KeyboardEventHandler<HTMLInputElement> = (event) => {
@@ -27,26 +33,44 @@ export default function Input(props: {
   };
 
   const onAddClick = async () => {
+    if (isLoading) return;
+    if (!inputValue.trim()) {
+      setError('You must enter your todo item');
+      inputRef.current.focus();
+      return;
+    }
     const title = inputValue;
     const newTodo = {
       title,
       checked: false,
       id: genId(),
     };
+    setIsLoading(true);
     await props.addTodo(newTodo);
+    setIsLoading(false);
     setInputValue('');
     inputRef.current.focus();
   };
   return (
-    <div className="flex items-center">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={onInputChange}
-        onKeyDown={onInputKeydown}
-        className="w-full px-4 py-3 my-4 font-thin rounded shadow"
-        ref={inputRef}
-      />
+    <div className="flex my-4">
+      <div className="flex flex-col w-full">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={onInputChange}
+          onKeyDown={onInputKeydown}
+          className={classNames(
+            'w-full px-4 py-3 font-thin rounded shadow',
+            error && 'border-red-500',
+            isLoading && 'bg-gray-300'
+          )}
+          ref={inputRef}
+        />
+        <span className="block h-3 mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
+          {error}
+        </span>
+      </div>
+      {isLoading && <Spinner className="ml-3 w-11 h-11" />}
       <AddBtn onClick={onAddClick} />
     </div>
   );
